2842105a4991c4f8a06a5f04786fde2adb691e60
Jillian <jilliangberger@gmail.com>
2014-12-03 15:47:10 -0700

    [83892286] venue_tablecreation

---
+++ db/migrate/20141203224626_create_venues.rb
@@ -0,0 +1,12 @@
+class CreateVenues < ActiveRecord::Migration
+  def change
+    create_table :venues do |t|
+      t.string :venue_name
+      t.string :location
+      t.string :contact_number
+      t.string :homepage_link
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/venues.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :venue do
+    venue_name "MyString"
+location "MyString"
+contact_number "MyString"
+homepage_link "MyString"
+  end
+
+end
