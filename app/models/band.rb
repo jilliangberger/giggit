2c9cde84566466bb9aa6d3f36623d1a518922439
Johnny Lamb <agnusjack@gmail.com>
2014-12-13 22:31:47 -0700

    added validations for band_name, home_town, genre.

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,7 +1,7 @@
 class Band < ActiveRecord::Base
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :band_name, :genre, :home_town, :email, presence: true, uniqueness: true
 
   has_many :band_members
   has_many :members
