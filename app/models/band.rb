df99511e9ce553322f4b217d121cc39fc4389d65
Johnny Lamb <agnusjack@gmail.com>
2014-12-14 23:11:43 -0700

    cleaned up code

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,7 +1,8 @@
 class Band < ActiveRecord::Base
   has_secure_password
 
-  validates :band_name, :genre, :home_town, :email, presence: true, uniqueness: true
+  validates :band_name, :genre, :home_town, :email, presence: true,
+    uniqueness: true
 
   has_many :band_members
   has_many :members
