696bdbf36c5a3f6bb2ee00edac69edd072880534
Johnny Lamb <agnusjack@gmail.com>
2014-12-15 21:52:04 -0700

    added validations for band and models/event

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,8 +1,9 @@
 class Band < ActiveRecord::Base
   has_secure_password
 
-  validates :band_name, :genre, :home_town, :email, presence: true,
-    uniqueness: true
+  validates :band_name, :genre, :home_town, presence: true
+
+  validates :email, uniqueness: true
 
   has_many :band_members
   has_many :members
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
 
   validates :date, :booking_agent_number, presence: true
-
+  #add a validation for band id and venue id
     belongs_to :bands
     belongs_to :members
 
